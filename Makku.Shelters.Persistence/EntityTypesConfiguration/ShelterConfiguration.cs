540405c902757f52291b35d92ecfeb2f4cf6435d
Roman <tvgs-krv@mail.ru>
2022-10-25 19:34:54 +0300

    [create] base project structure

---
+++ Makku.Shelters.Persistence/EntityTypesConfiguration/ShelterConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Makku.Shelters.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Makku.Shelters.Persistence.EntityTypesConfiguration
+{
+    public class ShelterConfiguration : IEntityTypeConfiguration<Shelter>
+    {
+        public void Configure(EntityTypeBuilder<Shelter> builder)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
