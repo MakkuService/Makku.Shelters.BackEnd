169c25ac2a0a3210e363a42d0c7b2b37f00d6117
Roman <tvgs-krv@mail.ru>
2022-10-25 20:02:50 +0300

    [add] mapping

---
+++ Makku.Shelters.Application/Common/Mappings/IMapWith.cs
@@ -0,0 +1,9 @@
+﻿using AutoMapper;
+
+namespace Makku.Shelters.Application.Common.Mappings
+{
+    public interface IMapWith<T>
+    {
+        void Mapping(Profile profile) => profile.CreateMap(typeof(T), GetType());
+    }
+}
