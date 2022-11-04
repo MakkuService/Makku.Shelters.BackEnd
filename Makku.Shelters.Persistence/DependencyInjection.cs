4e6e5f12955b1c5e83baabd58522df2a925f32f5
Roman <tvgs-krv@mail.ru>
2022-11-04 21:59:48 +0300

    [add] base DI

---
+++ Makku.Shelters.Persistence/DependencyInjection.cs
@@ -0,0 +1,21 @@
+﻿using Makku.Shelters.Application.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+
+namespace Makku.Shelters.Persistence
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddPersistence(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration["DbConnection"];
+            services.AddDbContext<SheltersDbContext>(opt =>
+            {
+                opt.UseNpgsql(connectionString);
+            });
+            services.AddScoped<ISheltersDbContext>(provider => provider.GetService<SheltersDbContext>());
+            return services;
+        }
+    }
+}
